dce651c133f3173379e9cad745702ef7b01370f8
EmilAvramov <e_avramov@zoho.eu>
2022-05-28 11:41:59 +0300

    5/28/2022

---
+++ JavaScript/Advanced & Web/Objects & Composition/calorieCount.js
@@ -0,0 +1,11 @@
+function calories(arr = []) {
+    var register = {};
+    for (let i = 0; i < arr.length; i += 2) {
+        if (i % 2 == 0) {
+            register[arr[i]] = Number(arr[i + 1]);
+        }
+    }
+    console.log(register);
+}
+
+calories(["Potato", "93", "Skyr", "63", "Cucumber", "18", "Milk", "42"]);
