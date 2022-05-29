4f6aa39e76cb4a8d4577bf6f18675772c93782e8
EmilAvramov <e_avramov@zoho.eu>
2022-05-29 19:25:14 +0300

    5/29/2022

--- JavaScript/Advanced & Web/DOM Introduction/Generate Report/report.js
+++ JavaScript/Advanced & Web/DOM Introduction/Generate Report/report.js
@@ -1,3 +1,19 @@
 function generateReport() {
-    //TODO
+    let headers = Array.from(document.querySelectorAll(`thead tr th input`));
+    let rows = document.querySelectorAll(`tbody tr`);
+
+    let output = [];
+    for (let i = 0; i < rows.length; i++) {
+        let current = {};
+
+        for (let j = 0; j < headers.length; j++) {
+            if (headers[j].checked) {
+                current[headers[j].name] = rows[i].children[j].textContent;
+            }
+        }
+
+        output.push(current);
+    }
+
+    document.getElementById("output").value = JSON.stringify(output);
 }
