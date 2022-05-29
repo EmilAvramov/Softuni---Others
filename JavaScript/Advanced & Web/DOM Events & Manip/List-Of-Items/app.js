c3f03f5db0a09367e167478ea45baf2418be672e
EmilAvramov <e_avramov@zoho.eu>
2022-05-29 21:07:58 +0300

    5/29/2022

--- JavaScript/Advanced & Web/DOM Events & Manip/List-Of-Items/app.js
+++ JavaScript/Advanced & Web/DOM Events & Manip/List-Of-Items/app.js
@@ -1,3 +1,12 @@
 function addItem() {
-    console.log('TODO:...');
+    var input = document.getElementById("newItemText");
+    var target = document.querySelector("input[type='button']");
+    var list = document.getElementById("items")
+    target.addEventListener("click", createNew(input, list));
+
+    function createNew(input, parent) {
+        var next = document.createElement("li");
+        next.textContent = input.value;
+        parent.appendChild(next);
+    }
 }
