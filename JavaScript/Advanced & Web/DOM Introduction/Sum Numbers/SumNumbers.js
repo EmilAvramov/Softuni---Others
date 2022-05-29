5396093eeea56f36ae845c612ef340c55f90bcfd
EmilAvramov <e_avramov@zoho.eu>
2022-05-29 11:35:47 +0300

    5/29/2022

--- JavaScript/Advanced & Web/DOM Introduction/Sum Numbers/SumNumbers.js
+++ JavaScript/Advanced & Web/DOM Introduction/Sum Numbers/SumNumbers.js
@@ -1,3 +1,5 @@
 function calc() {
-    // TODO: sum = num1 + num2
+    num1 = Number(document.getElementById("num1").value)
+    num2 = Number(document.getElementById("num2").value)
+    document.getElementById("sum").value = num1 + num2
 }
