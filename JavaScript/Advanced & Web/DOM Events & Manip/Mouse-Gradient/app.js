1061d3bcd82d74e17486f873b7f641d4be24028e
EmilAvramov <e_avramov@zoho.eu>
2022-05-30 13:42:49 +0300

    5/30/2022

--- JavaScript/Advanced & Web/DOM Events & Manip/Mouse-Gradient/app.js
+++ JavaScript/Advanced & Web/DOM Events & Manip/Mouse-Gradient/app.js
@@ -1,3 +1,17 @@
 function attachGradientEvents() {
-    console.log('TODO:...');
+    var target = document.getElementById("gradient");
+    var result = document.getElementById("result");
+
+    target.addEventListener("mousemove", logMouse);
+    target.addEventListener("mouseout", mouseOut);
+
+    function logMouse(event) {
+        let percentage = event.offsetX / (event.target.clientWidth - 1);
+        percentage = Math.trunc(percentage * 100);
+        result.textContent = `${percentage}%`;
+    }
+
+    function mouseOut() {
+        result.textContent = "";
+    }
 }
