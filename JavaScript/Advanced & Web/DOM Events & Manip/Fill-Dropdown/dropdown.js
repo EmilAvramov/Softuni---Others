4d769f584f36f77b88b99b382c17a7db74dd7828
EmilAvramov <e_avramov@zoho.eu>
2022-05-30 18:02:28 +0300

    5/30/2022

--- JavaScript/Advanced & Web/DOM Events & Manip/Fill-Dropdown/dropdown.js
+++ JavaScript/Advanced & Web/DOM Events & Manip/Fill-Dropdown/dropdown.js
@@ -1,3 +1,17 @@
 function addItem() {
-    console.log('TODO:...');
+    var output = document.getElementById("menu");
+    var textInput = document.getElementById("newItemText");
+    var valueInput = document.getElementById("newItemValue");
+    var button = document.querySelectorAll("input[type='button']")[0];
+
+    button.addEventListener("click", create(textInput, valueInput));
+
+    function create(text, val) {
+        let element = document.createElement("option");
+        element.textContent = text.value;
+        element.value = val.value;
+        output.appendChild(element);
+        text.value = "";
+        val.value = "";
+    }
 }
--- JavaScript/Advanced & Web/DOM Events & Manip/Locked-Profile/app.js
+++ JavaScript/Advanced & Web/DOM Events & Manip/Locked-Profile/app.js
@@ -1,3 +1,16 @@
 function lockedProfile() {
-    console.log('TODO...')
+    let buttons = [...document.querySelectorAll("button")];
+    buttons.forEach((button) => button.addEventListener("click", addEvent));
+
+    function addEvent(event) {
+        if (event.target.parentNode.children[4].checked === true) {
+            if (event.target.textContent === "Show more") {
+                event.target.parentNode.children[9].style.display = "block";
+                event.target.textContent = "Hide it";
+            } else {
+                event.target.parentNode.children[9].style.display = "none";
+                event.target.textContent = "Show more";
+            }
+        }
+    }
 }
