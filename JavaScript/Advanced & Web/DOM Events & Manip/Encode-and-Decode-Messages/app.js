f57571c50cac49f14e6b433878cfe4458064ee5e
EmilAvramov <e_avramov@zoho.eu>
2022-05-31 13:54:44 +0300

    5/31/2022

--- JavaScript/Advanced & Web/DOM Events & Manip/Encode-and-Decode-Messages/app.js
+++ JavaScript/Advanced & Web/DOM Events & Manip/Encode-and-Decode-Messages/app.js
@@ -1,28 +1,28 @@
 function encodeAndDecodeMessages() {
-    var encodeBox = document.getElementsByTagName("textarea")[0];
-    var encodeBtn = document.getElementsByTagName("button")[0];
-    var decodeBox = document.getElementsByTagName("textarea")[1];
-    var decodeBtn = document.getElementsByTagName("button")[1];
+    var encodeBox = document.querySelectorAll("textarea")[0];
+    var encodeBtn = document.querySelectorAll("button")[0];
+    var decodeBox = document.querySelectorAll("textarea")[1];
+    var decodeBtn = document.querySelectorAll("button")[1];
 
-    encodeBtn.addEventListener("click", encode(encodeBox, decodeBox));
-    decodeBtn.addEventListener("click", decode(decodeBox))
+    encodeBtn.addEventListener("click", encode);
+    decodeBtn.addEventListener("click", decode);
 
-    function encode(source, target) {
-        let message = source.value;
+    function encode() {
+        let message = encodeBox.value;
         let newMsg = "";
         for (let i = 0; i < message.length; i++) {
             newMsg += String.fromCodePoint(message[i].charCodeAt(0) + 1);
         }
-        target.value = newMsg;
-        source.value = "";
+        decodeBox.value = newMsg;
+        encodeBox.value = "";
     }
 
-    function decode(source) {
-        let message = source.value;
+    function decode() {
+        let message = decodeBox.value;
         let newMsg = "";
         for (let i = 0; i < message.length; i++) {
             newMsg += String.fromCodePoint(message[i].charCodeAt(0) - 1);
         }
-        source.value = newMsg;
+        decodeBox.value = newMsg;
     }
 }
