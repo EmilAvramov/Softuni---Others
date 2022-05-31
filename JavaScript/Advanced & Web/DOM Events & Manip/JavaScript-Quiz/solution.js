a12c96ad3f1564732e31a191e9e342e3ff21bdd4
EmilAvramov <e_avramov@zoho.eu>
2022-05-31 17:46:21 +0300

    5/31/2022

--- JavaScript/Advanced & Web/DOM Events & Manip/JavaScript-Quiz/solution.js
+++ JavaScript/Advanced & Web/DOM Events & Manip/JavaScript-Quiz/solution.js
@@ -1,3 +1,44 @@
 function solve() {
-  //TODO...
+    var resultBox = document.querySelector("#results");
+    var resultTxt = document.querySelector("#results li h1");
+    var sections = document.querySelectorAll("section");
+    var answers = document.querySelectorAll(".answer-text");
+
+    let answerCount = 0;
+    let currentSection = 1;
+
+    for (let answer of answers) {
+        answer.addEventListener("click", nextPage);
+    }
+
+    function nextPage(event) {
+        const rightAnswers = [
+            "onclick",
+            "JSON.stringify()",
+            "A programming API for HTML and XML documents",
+        ];
+        if (rightAnswers.includes(event.target.textContent)) {
+            answerCount += 1;
+        }
+        if (currentSection === 1) {
+            sections[0].style.display = "none"
+            sections[1].style.display = "block"
+        } else if (currentSection === 2) {
+            sections[1].style.display = "none"
+            sections[2].style.display = "block"
+        } else if (currentSection === 3) {
+            sections[2].style.display = "none"
+            outcome();
+        }
+        currentSection++;
+    }
+
+    function outcome() {
+        resultBox.style.display = "block";
+        if (answerCount === 3) {
+            resultTxt.textContent = "You are recognized as top JavaScript fan!";
+        } else {
+            resultTxt.textContent = `You have ${answerCount} right answers`;
+        }
+    }
 }
