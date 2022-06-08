3d47f69f1ef59731f799cf412ae88117daaa5f34
EmilAvramov <e_avramov@zoho.eu>
2022-06-08 12:02:36 +0300

    6/8/2022

--- JavaScript/Advanced & Web/Classes & Attributes/11. View-Model/app.js
+++ JavaScript/Advanced & Web/Classes & Attributes/11. View-Model/app.js
@@ -1,8 +1,32 @@
 class Textbox {
-    //TODO
+    constructor(selector, regex) {
+        this._value = "";
+        this._elements = document.querySelectorAll(selector);
+        this._invalidSymbols = regex;
+    }
+    isValid() {
+        this._invalidSymbols.test(this.value) ? false : true;
+    }
+
+    get value() {
+        return this._value;
+    }
+
+    set value(input) {
+        this._elements = this._elements.map(
+            (element) => (element.value = input)
+        );
+        this._value = input;
+    }
+
+    get elements() {
+        return this._elements;
+    }
 }
 
-let textbox = new Textbox(".textbox",/[^a-zA-Z0-9]/);
-let inputs = document.getElementsByClassName('.textbox');
+let textbox = new Textbox(".textbox", /[^a-zA-Z0-9]/);
+let inputs = document.getElementsByClassName(".textbox");
 
-inputs.addEventListener('click',function(){console.log(textbox.value);});
+inputs.addEventListener("click", function () {
+    console.log(textbox.value);
+});
