1321a9b2c5354a5d5a6e55bd9f066a3e51c72992
EmilAvramov <e_avramov@zoho.eu>
2022-06-10 16:22:02 +0300

    6/10/2022

--- SQL/Data Definition And Data Types/minions.sql
+++ SQL/Data Definition And Data Types/minions.sql
@@ -41,6 +41,69 @@ VALUES
 DELETE FROM minions;
 
 -- 5
-
 DROP TABLE minions;
 DROP TABLE towns;
+
+-- 6
+CREATE TABLE people (
+    id INT AUTO_INCREMENT,
+    name NVARCHAR(200) NOT NULL,
+    picture VARBINARY(2000),
+    height REAL(3, 2),
+    weight NUMERIC(5, 2),
+    gender ENUM('m', 'f') NOT NULL,
+    birthdate DATE NOT NULL,
+    biography NVARCHAR(8000),
+    PRIMARY KEY (id)
+);
+
+INSERT INTO people
+(name, gender, birthdate)
+VALUES
+(
+       'First Name',
+       'M',
+       '2000-01-01'
+),
+(
+       'Second Name',
+       'F',
+       '2001-05-03'
+),
+(
+       'Third Name',
+       'F',
+       '2005-07-08'
+),
+(
+       'Fourth Name',
+       'F',
+       '2007-03-05'
+),
+(
+       'Fifth Name',
+       'M',
+       '2003-08-09'
+);
+
+-- 7
+
+CREATE TABLE users (
+    id BIGINT AUTO_INCREMENT NOT NULL,
+    username VARCHAR(30) NOT NULL UNIQUE,
+    password VARCHAR(26) NOT NULL,
+    profile_picture VARBINARY(8000),
+    last_login_time TIME,
+    is_deleted BIT,
+    PRIMARY KEY (id)
+);
+
+
+INSERT INTO users 
+(username, password) 
+VALUES 
+("John Doe", "aoe"), 
+("Mike Doe", "m_doe"), 
+("Jane Doe", "jdoe"),
+("Nicky Doe", "ane"), 
+("Nana Doe", "n_doe");
--- myConnection.session.sql
+++ myConnection.session.sql
@@ -1,4 +1,23 @@
 USE minions;
 
-DROP TABLE minions;
-DROP TABLE towns;
+DROP TABLE users;
+
+CREATE TABLE users (
+    id BIGINT AUTO_INCREMENT NOT NULL,
+    username VARCHAR(30) NOT NULL UNIQUE,
+    password VARCHAR(26) NOT NULL,
+    profile_picture VARBINARY(8000),
+    last_login_time TIME,
+    is_deleted BIT,
+    PRIMARY KEY (id)
+);
+
+
+INSERT INTO users 
+(username, password) 
+VALUES 
+("John Doe", "aoe"), 
+("Mike Doe", "m_doe"), 
+("Jane Doe", "jdoe"),
+("Nicky Doe", "ane"), 
+("Nana Doe", "n_doe");
