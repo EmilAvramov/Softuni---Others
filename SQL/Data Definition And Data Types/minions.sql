16da42e3f79b3acb1ba4dd9e817d19ac43ceeed2
EmilAvramov <e_avramov@zoho.eu>
2022-06-10 15:38:54 +0300

    6/10/2022

--- SQL/Data Definition And Data Types/minions.sql
+++ SQL/Data Definition And Data Types/minions.sql
@@ -17,5 +17,30 @@ CREATE TABLE towns (
 
 -- 2
 
-ALTER TABLE minions
+ALTER TABLE towns
 RENAME COLUMN town_id to id;
+
+ALTER TABLE minions
+ADD COLUMN town_id INT NOT NULL;
+
+ALTER TABLE minions
+ADD FOREIGN KEY (town_id)
+REFERENCES towns (id);
+
+-- 3
+INSERT INTO towns (id, name)
+VALUES (1, "Sofia"), (2, "Plovdiv"), (3, "Varna");
+
+INSERT INTO minions (id, name, age, town_id)
+VALUES 
+(1, "Kevin", 22, 1), 
+(2, "Bob", 15, 3), 
+(3, "Steward", NULL, 2);
+
+-- 4
+DELETE FROM minions;
+
+-- 5
+
+DROP TABLE minions;
+DROP TABLE towns;
