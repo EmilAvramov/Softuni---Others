9267a66858a4603c2c73e46bdf3d826883ea4b9b
EmilAvramov <e_avramov@zoho.eu>
2022-06-11 16:21:59 +0300

    6/11/2022

--- SQL/Basic CRUD/Exercise.sql
+++ SQL/Basic CRUD/Exercise.sql
@@ -26,3 +26,21 @@ SELECT DISTINCT(salary) FROM employees;
 -- 7
 SELECT * FROM employees
 WHERE job_title = "Sales Representative";
+
+-- 8
+SELECT first_name, last_name, job_title FROM employees
+WHERE salary >= 20000 AND salary <= 30000
+ORDER BY employee_id;
+
+-- 9
+SELECT CONCAT(first_name, " ", middle_name, " ", last_name) AS "Full Name"
+FROM employees
+WHERE salary = 25000
+    OR salary = 14000
+    OR salary = 12500
+    OR salary = 23600;
+
+-- 10
+SELECT first_name, last_name
+FROM employees
+WHERE manager_id IS NULL;
--- myConnection.session.sql
+++ myConnection.session.sql
@@ -1,2 +1,3 @@
-SELECT * FROM employees
-WHERE job_title = "Sales Representative";
+SELECT first_name, last_name
+FROM employees
+WHERE manager_id IS NULL;
