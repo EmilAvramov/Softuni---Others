74a60b7af96daa4ab198647a2d696695be4095ff
EmilAvramov <e_avramov@zoho.eu>
2022-06-13 16:19:22 +0300

    6/13/2022

--- Databases/MySQL/Table Relations/Exercise.sql
+++ Databases/MySQL/Table Relations/Exercise.sql
@@ -167,3 +167,53 @@ CREATE TABLE order_items (
     CONSTRAINT fk_order_ids FOREIGN KEY (order_id) REFERENCES orders (order_id),
     CONSTRAINT fk_items FOREIGN KEY (item_id) REFERENCES items (item_id)
 );
+
+-- 5
+CREATE DATABASE university;
+
+USE university;
+
+CREATE TABLE majors (
+    major_id INT(11) NOT NULL,
+    name VARCHAR(50) NOT NULL,
+    CONSTRAINT pk_majors PRIMARY KEY (major_id)
+);
+
+CREATE TABLE students (
+    student_id INT(11) NOT NULL,
+    student_number VARCHAR(12) NOT NULL,
+    student_name VARCHAR(50) NOT NULL,
+    major_id INT(11) NOT NULL,
+    CONSTRAINT fk_students PRIMARY KEY (student_id),
+    CONSTRAINT fk_students FOREIGN KEY (major_id) REFERENCES majors (major_id)
+);
+
+CREATE TABLE payments (
+    payment_id INT(11) NOT NULL,
+    payment_date DATE NOT NULL,
+    payment_amount DECIMAL(8, 2) NOT NULL,
+    student_id INT(11) NOT NULL,
+    CONSTRAINT pk_payments PRIMARY KEY (payment_id),
+    CONSTRAINT fk_payments FOREIGN KEY (student_id) REFERENCES students(student_id)
+);
+
+CREATE TABLE subjects (
+    subject_id INT(11) NOT NULL,
+    subject_name VARCHAR(50) NOT NULL,
+    CONSTRAINT pk_subjects PRIMARY KEY (subject_id)
+);
+
+CREATE TABLE agenda (
+    student_id INT(11) NOT NULL,
+    subject_id INT(11) NOT NULL,
+    CONSTRAINT pk_agenda PRIMARY KEY (student_id, subject_id),
+    CONSTRAINT fk_agenda_student FOREIGN KEY (student_id) REFERENCES students (student_id),
+    CONSTRAINT fk_agenda_subject FOREIGN KEY (subject_id) REFERENCES subjects (subject_id)
+);
+
+-- 6
+SELECT m.mountain_range, p.peak_name, p.elevation
+FROM mountains AS m
+JOIN peaks as p
+ON m.id = p.mountain_id AND m.mountain_range = "Rila"
+ORDER BY p.elevation DESC;
--- myConnection.session.sql
+++ myConnection.session.sql
@@ -1,9 +1,5 @@
-
-
-CREATE TABLE order_items (
-    order_id INT(11) NOT NULL,
-    item_id INT(11) NOT NULL,
-    CONSTRAINT pk_order_items PRIMARY KEY (order_id, item_id),
-    CONSTRAINT fk_order_ids FOREIGN KEY (order_id) REFERENCES orders (order_id),
-    CONSTRAINT fk_items FOREIGN KEY (item_id) REFERENCES items (item_id)
-);
+SELECT m.mountain_range, p.peak_name, p.elevation
+FROM mountains AS m
+JOIN peaks as p
+ON m.id = p.mountain_id AND m.mountain_range = "Rila"
+ORDER BY p.elevation DESC;
