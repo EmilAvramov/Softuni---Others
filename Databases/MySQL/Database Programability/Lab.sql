24c3e1c037f8167a45c9bc4cbb517b7cf7c19808
EmilAvramov <e_avramov@zoho.eu>
2022-06-16 15:30:55 +0300

    6/16/2022

--- Databases/MySQL/Database Programability/Lab.sql
+++ Databases/MySQL/Database Programability/Lab.sql
@@ -23,9 +23,51 @@ BEGIN
 END
 
 -- 3
+
+-- No transaction
+
+-- CREATE PROCEDURE usp_raise_salary_by_id (id INT)
+-- BEGIN
+--     UPDATE employees AS e
+--     SET e.salary = e.salary * 1.05
+--     WHERE e.employee_id = id;
+-- END
+
+-- With transaction
 CREATE PROCEDURE usp_raise_salary_by_id (id INT)
 BEGIN
-    UPDATE employees AS e
-    SET e.salary = e.salary * 1.05
-    WHERE e.employee_id = id;
-END
+    START TRANSACTION;
+    IF ((SELECT COUNT(*) FROM employees WHERE employee_id = id) > 0)
+    THEN
+        UPDATE employees SET salary = salary * 1.05 WHERE employee_id = id;
+        COMMIT;
+    ELSE 
+        ROLLBACK;
+    END IF;
+END;
+
+-- 4
+CREATE TABLE deleted_employees(
+    employee_id INT AUTO_INCREMENT NOT NULL,
+    first_name NVARCHAR(50) NOT NULL,
+    last_name NVARCHAR(50) NOT NULL,
+    middle_name NVARCHAR(50) NOT NULL,
+    job_title NVARCHAR(100) NOT NULL,
+    department_id INT NOT NULL,
+    salary NUMERIC(8, 2) NOT NULL,
+    CONSTRAINT pk_de PRIMARY KEY (employee_id)
+);
+
+CREATE TRIGGER trigger_employee BEFORE DELETE ON employees
+FOR EACH ROW
+    INSERT INTO deleted_employees(
+        first_name, last_name, middle_name, job_title, department_id, salary
+    )
+    VALUES
+    (
+    OLD.first_name, 
+    OLD.last_name, 
+    OLD.middle_name, 
+    OLD.job_title,
+    OLD.department_id, 
+    OLD.salary);
--- myConnection.session.sql
+++ myConnection.session.sql
@@ -1,8 +1,24 @@
 CREATE TABLE deleted_employees(
-    employee_id INT NOT NULL,
+    employee_id INT AUTO_INCREMENT NOT NULL,
     first_name NVARCHAR(50) NOT NULL,
     last_name NVARCHAR(50) NOT NULL,
+    middle_name NVARCHAR(50) NOT NULL,
     job_title NVARCHAR(100) NOT NULL,
-    SALARY NUMERIC(8, 2) NOT NULL,
+    department_id INT NOT NULL,
+    salary NUMERIC(8, 2) NOT NULL,
     CONSTRAINT pk_de PRIMARY KEY (employee_id)
 );
+
+CREATE TRIGGER trigger_employee BEFORE DELETE ON employees
+FOR EACH ROW
+    INSERT INTO deleted_employees(
+        first_name, last_name, middle_name, job_title, department_id, salary
+    )
+    VALUES
+    (
+    OLD.first_name, 
+    OLD.last_name, 
+    OLD.middle_name, 
+    OLD.job_title,
+    OLD.department_id, 
+    OLD.salary);
