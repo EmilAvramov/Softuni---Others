27e300f07d2e7bb22d3eeb8fac6f6b190eca2717
EmilAvramov <e_avramov@zoho.eu>
2022-06-20 14:05:57 +0300

    6/20/2022

--- JavaScript/Applications/AJAX/06. Bus-Schedule/app.js
+++ JavaScript/Applications/AJAX/06. Bus-Schedule/app.js
@@ -10,24 +10,37 @@ function solve() {
     let start = document.getElementById("depart");
     let end = document.getElementById("arrive");
     let data = getData();
-    let index = 0;
-    // The json is a linked list 
+    let c_stop = "";
+    let next = "";
+
+    // The json is a linked list
     async function depart() {
-        let stop = await data.then((value) => Object.entries(value)[index]);
+        start.disabled = true;
+        end.disabled = false;
+        let stop = await data.then((value) => Object.entries(value));
         if (info.textContent === "Not Connected") {
-            stop = await data.then((value) => Object.entries(value)[15]);
-            info.textContent = stop[1].name;
-            index++;
-        } else if (index === 14) {
+            c_stop = stop[15][0];
+            info.textContent = `Next stop ${c_stop}`;
+            next = stop[15][1].next;
+        } else if (next === "depot") {
             info.textContent = "Error";
         } else {
-            info.textContent = stop[1].name;
-            index++;
+            for (let i = 0; i < Object.values(stop).length; i++) {
+                if (Object.values(stop)[i][0] === next) {
+                    idx = Object.values(stop)[i][0];
+                    c_stop = Object.values(stop)[i][1].name;
+                    info.textContent = `Next stop ${c_stop}`;
+                    next = Object.values(stop)[i][1].next;
+                    break;
+                }
+            }
         }
-    } 
+    }
 
     function arrive() {
-        console.log("Arrive TODO...");
+        start.disabled = false;
+        end.disabled = true;
+        info.textContent = `Arriving at ${c_stop}`;
     }
 
     return {
