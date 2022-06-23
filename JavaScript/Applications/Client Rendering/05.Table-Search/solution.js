6de55c404a1f4a3ea8791e5472844012bcdb84a8
EmilAvramov <e_avramov@zoho.eu>
2022-06-24 00:35:03 +0300

    6/24/2022

--- JavaScript/Applications/Client Rendering/05.Table-Search/solution.js
+++ JavaScript/Applications/Client Rendering/05.Table-Search/solution.js
@@ -1,8 +1,21 @@
-function solve() {
-   document.querySelector('#searchBtn').addEventListener('click', onClick);
+/* eslint-disable import/extensions */
+/* eslint-disable import/no-relative-packages */
+import { render } from '../node_modules/lit-html/lit-html.js';
+import * as template from './templates.js';
 
-   function onClick() {
-      //   TODO:
+const url = 'http://localhost:3030/jsonstore/advanced/table';
+const root = document.querySelector('tbody');
+const btn = document.getElementById('searchBtn');
+const input = document.getElementById('searchField');
 
-   }
+async function getData() {
+	const request = await fetch(url);
+	return request.json();
 }
+
+render(template.load(getData()), root);
+
+btn.addEventListener('click', () => {
+	render(template.selected(getData(), input.value), root);
+	input.value = '';
+});
---
+++ JavaScript/Applications/Client Rendering/05.Table-Search/templates.js
@@ -0,0 +1,52 @@
+/* eslint-disable import/extensions */
+/* eslint-disable import/no-relative-packages */
+import { html } from '../node_modules/lit-html/lit-html.js';
+import { until } from '../node_modules/lit-html/directives/until.js';
+
+export const load = (data) =>
+	html`${until(
+		data.then((res) =>
+			Object.values(res).map(
+				// eslint-disable-next-line no-underscore-dangle
+				(el) => html`<tr>
+					<td>${el.firstName} ${el.lastName}</td>
+					<td>${el.email}</td>
+					<td>${el.course}</td>
+				</tr>`
+			)
+		)
+	)}`;
+
+export const selected = (data, srch) =>
+	html`${until(
+		data.then((res) =>
+			Object.values(res).map(
+				// eslint-disable-next-line no-underscore-dangle
+				(el) => {
+					if (
+						el.firstName
+							.toLowerCase()
+							.includes(srch.toLowerCase()) ||
+						el.lastName
+							.toLowerCase()
+							.includes(srch.toLowerCase()) ||
+						el.email.toLowerCase().includes(srch.toLowerCase()) ||
+						el.course.toLowerCase().includes(srch.toLowerCase())
+					) {
+						return html`<tr>
+							<td class="select">
+								${el.firstName} ${el.lastName}
+							</td>
+							<td class="select">${el.email}</td>
+							<td class="select">${el.course}</td>
+						</tr>`;
+					}
+					return html`<tr>
+						<td>${el.firstName} ${el.lastName}</td>
+						<td>${el.email}</td>
+						<td>${el.course}</td>
+					</tr>`;
+				}
+			)
+		)
+	)}`;
