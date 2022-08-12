b4ed7321c66987d869e1ca2eecaaa5cc4abf9e43
EmilAvramov <e_avramov@zoho.eu>
2022-08-12 10:26:41 +0300

    8/12/2022

---
+++ JavaScript/Backend/Templating/models/cube.js
@@ -0,0 +1,9 @@
+class Cube {
+    constructor(id, name, description, url, level) {
+        this.id = id
+        this.name = name
+        this.description = description
+        this.url = url
+        this.level = level
+    }
+}
