2deb94d732b36e82ebe924ac7debdbe430711123
EmilAvramov <e_avramov@zoho.eu>
2022-08-15 09:54:32 +0300

    8/15/2022

---
+++ JavaScript/Backend/Session and Auth Workshop/models/cube.js
@@ -0,0 +1,40 @@
+const mongoose = require('mongoose');
+
+const cubeSchema = new mongoose.Schema({
+	id: mongoose.Types.ObjectId,
+	name: {
+		type: String,
+		required: [true, 'This field is required'],
+	},
+	description: {
+		type: String,
+		required: [true, 'This field is required'],
+		maxLength: 120,
+	},
+	imageUrl: {
+		type: String,
+		required: [true, 'This field is required'],
+		validate: {
+			validator: function () {
+				return this.imageUrl.startsWith('http');
+			},
+			message: 'Image url should be link',
+		},
+	},
+	difficultyLevel: {
+		type: Number,
+		required: [true, 'This field is required'],
+		min: 1,
+		max: 6,
+	},
+	accessories: [
+		{
+			type: mongoose.Types.ObjectId,
+			ref: 'Accessory',
+		},
+	],
+});
+
+const Cube = mongoose.model('Cube', cubeSchema);
+
+module.exports = Cube;
