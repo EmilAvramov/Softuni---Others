0d6af9618872bafcefd5bc95c4be2c386f291343
EmilAvramov <e_avramov@zoho.eu>
2022-10-21 11:43:23 +0300

    10/21/2022

--- Python/Advanced/multidimensional_lists/snake_moves.py
+++ Python/Advanced/multidimensional_lists/snake_moves.py
@@ -1,18 +1,34 @@
+from collections import deque
+
+
 [rows, cols] = [int(i) for i in input().split(" ")]
 snake = input()
-matrix = []
 index = 0
 
-for i in range(rows):
-    if i % 2 == 0:
-        for j in range(cols):
-            matrix.append(snake[0:cols])
-            print(index)
-            break
-    else:
-        for j in range(cols):
-            matrix.append(snake[cols::-1])
-            break
+for row in range(rows):
+    matrix_row = deque()
+    for col in range(cols):
+        if index == len(snake):
+            index = 0
+        if row % 2 == 0:
+            matrix_row.append(snake[index])
+        else:
+            matrix_row.appendleft(snake[index])
+        index += 1
+    print("".join(matrix_row))
 
-for row in range(len(matrix)):
-    print("".join(str(x) for x in matrix[row]))
+# for i in range(rows):
+#     sub = []
+#     index = 0
+#     for char in last:
+#         sub.append(char)
+#         index = snake.index(char)
+#     mid = snake[index + 1 : cols + 1]
+#     last = mid + snake[: (cols - len(mid))]
+#     if i % 2 == 0:
+#         matrix.append(sub)
+#     else:
+#         matrix.append(reversed(sub))
+
+# for row in range(len(matrix)):
+#     print("".join(str(x) for x in matrix[row]))
